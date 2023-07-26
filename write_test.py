60f4cab7152f254df35a31427a210ef142522537
Min Ku Kang <tominku@gmail.com>
2023-07-26 17:05:41 +0900

    ...

---
+++ write_test.py
@@ -0,0 +1,8 @@
+import os
+#to get the current working directory
+import matplotlib.pyplot as plt
+import numpy as np
+
+with open('test.txt', 'w') as f:
+    f.write("test\n")
+    f.write("test2")
