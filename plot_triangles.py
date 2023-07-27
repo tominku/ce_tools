f05cee88fc37702eca7e9c64821d4eed68cdf9ff
Min Ku Kang <tominku@gmail.com>
2023-07-27 17:44:44 +0900

    ...

--- plot_triangles.py
+++ plot_triangles.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 directory = os.getcwd()
 
-exp_name = "A.1"
+exp_name = "hand_drawing.1"
 poly_file = directory + "/%s.node" % exp_name
 
 f = open(poly_file, 'r')
@@ -15,6 +15,7 @@ fig, ax = plt.subplots()
 
 x_coords = []
 y_coords = []
+list_is_boundary = []
 for i in range(num_vertices):
     line = f.readline().strip()
     temp = line.split()
@@ -22,8 +23,13 @@ for i in range(num_vertices):
     y_coord = float(temp[2])
     x_coords.append(x_coord)
     y_coords.append(y_coord)
+    is_boundary = int(temp[3])
     print_str = "%f %f\n" % (x_coord, y_coord)
+    list_is_boundary.append(is_boundary)
     print(print_str)    
+
+    if is_boundary:            
+        ax.scatter(x_coord, y_coord, c='red', zorder=2)    
 
 
 ele_file = directory + "/%s.ele" % exp_name
@@ -35,7 +41,7 @@ print('num_elements: %d' % num_elements)
 
 #line = f.readline().strip()
 # num_edges = int(line.split(" ")[0])
-# print('num_edges: ', num_edges)
+# print('num_edges:  ', num_edges)
 
 triangles = []
 for i in range(num_elements):
@@ -46,13 +52,40 @@ for i in range(num_elements):
     print(triangle)
     #print("triangle: %d %d %d" % (int(temp[0]), int(temp[1]), int(temp[2])))
 
+
 import matplotlib.tri as mtri
 
 triang = mtri.Triangulation(x_coords, y_coords, triangles)
-plt.title("triangular grid")
-plt.triplot(triang, 'ko-')
+plt.title("Mesh")
+#ax.triplot(triang, 'ko-')
+ax.triplot(triang, 'ko-', zorder=1)
+
+plt.xlim([0, 1.0])
+plt.ylim([0, 1.0])
+
+# node_file = directory + "/%s.node" % exp_name
+
+# f = open(edge_file, 'r')
+# first_line = f.readline().strip()
+# num_edges = int(first_line.split()[0])
+# print('num_edges: %d' % num_edges)
+
+# for i in range(num_edges):
+#     line = f.readline().strip()
+#     temp = line.split()    
+#     is_boundary = int(temp[3])
+#     if is_boundary:
+#         vertex1_index = int(temp[1])
+#         vertex2_index = int(temp[2])
+#         print("is_boundary: %d" % is_boundary)
+#         boundary_points_x = [x_coords[vertex1_index], x_coords[vertex2_index]]
+#         boundary_points_y = [y_coords[vertex1_index], y_coords[vertex2_index]]
+#         ax.scatter(boundary_points_x, boundary_points_y, c='red', zorder=2)
+#     #temp[1]
+#     #temp[2]
+    
+
+
 plt.show()
 
-# # plt.xlim([0, 1.0])
-# # plt.ylim([0, 1.0])
 # plt.show()
--- polygon_hand_drawing.py
+++ polygon_hand_drawing.py
@@ -5,7 +5,7 @@ import numpy as np
 fig2, ax2 = plt.subplots()
 fig2.show()
 
-selector2 = PolygonSelector(ax2, lambda *args: None)
+selector = PolygonSelector(ax2, lambda *args: None)
 
 print("Click on the figure to create a polygon.")
 print("Press the 'esc' key to start a new polygon.")
@@ -14,20 +14,22 @@ print("Try holding the 'ctrl' key to move a single vertex.")
 
 plt.show()
 
-num_vertices = len(selector2.verts)
-first_line = "%d\n" % num_vertices
+num_vertices = len(selector.verts)
+first_line = "%d 2 0 0\n" % num_vertices
 i = 1
-with open('verts.txt', 'w') as f:
+with open('hand_drawing.poly', 'w') as f:
+    
+    # vertices
     f.write(first_line)
-    for vert in selector2.verts:
+    for vert in selector.verts:
         line = "%d %f %f\n" % (i, vert[0], vert[1])
         f.write(line)        
         print(line)
         i += 1
 
-num_edges = num_vertices
-first_line = "%d\n" % num_edges
-with open('edges.txt', 'w') as f:
+    # edges
+    num_edges = num_vertices
+    first_line = "%d 0\n" % num_edges
     f.write(first_line)
     for i in range(num_edges):
         edge_v1_index = i+1
@@ -37,7 +39,9 @@ with open('edges.txt', 'w') as f:
         line = "%d %d %d\n" % (i+1, edge_v1_index, edge_v2_index)
         f.write(line)        
         print(line)    
-        
+    
+    # holes
+    f.write("0")
     
 # verts = np.array(selector2.verts)
 # with open('verts.poly', 'wb') as zf:
